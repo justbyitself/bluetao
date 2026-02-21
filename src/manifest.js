4a2025072804bec10780c28df7de9cbfa84e8b5b
justbyitself <justbyitself@proton.me>
2026-02-21 10:23:29 +0100

    Add numeric slots

--- src/manifest.js
+++ src/manifest.js
@@ -27,4 +27,10 @@ export const slots = [
   ['min'],
   ['mod'],
   ['quotient'],
+  ['abs'],
+  ['isEqualTo'],
+  ['isGreaterThan'],
+  ['isLesserThan'],
+  ['isGreaterThanOrEqualTo'],
+  ['isLesserThanOrEqualTo'],
 ]
